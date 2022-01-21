1768eb474cea7e8f63bbd4b7b51b0bc0202f3840
Aniket Kanere <aniket15304@gmail.com>
2022-01-21 15:15:41 +0530

    updated client

--- next.config.js
+++ next.config.js
@@ -1,9 +1,19 @@
-module.exports ={
-    images:{
-        domains:["links.papareact.com","fakestoreapi.com", "upload.wikimedia.org"],
-    },
+module.exports = {
+  images: {
+    domains: [
+      'links.papareact.com',
+      'fakestoreapi.com',
+      'upload.wikimedia.org',
+    ],
+  },
 
-    env:{
-        stripe_public_key: process.env.STRIPE_PUBLIC_KEY,
-    }
-}
+  env: {
+    stripe_public_key: process.env.STRIPE_PUBLIC_KEY,
+    GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
+    GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
+    STRIPE_PUBLIC_KEY: process.env.STRIPE_PUBLIC_KEY,
+    STRIPE_SECRET_KEY: process.env.STRIPE_SECRET_KEY,
+    NEXTAUTH_URL: process.env.NEXTAUTH_URL,
+    HOST: process.env.HOST,
+  },
+};
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -4,6 +4,7 @@ import '../styles/globals.css';
 import { SessionProvider } from 'next-auth/react';
 
 const MyApp = ({ Component, pageProps }) => {
+  console.log(process.env.GOOGLE_CLIENT_ID);
   return (
     <SessionProvider session={pageProps.session}>
       <Provider store={store}>
