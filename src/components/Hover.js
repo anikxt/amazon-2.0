d6caeff66c7b30699a0301b5cc2fa8b996ec258d
Aniket Kanere <aniket15304@gmail.com>
2022-01-21 14:00:22 +0530

    updated client

--- src/components/Hover.js
+++ src/components/Hover.js
@@ -1,21 +1,21 @@
-import Image from "next/image";
-import { useSelector } from "react-redux";
-import Currency from "react-currency-formatter";
-import { selectItems, selectTotal } from "../slices/basketSlice";
-import { useSession } from "next-auth/client";
-import { groupBy } from "lodash";
+import Image from 'next/image';
+import { useSelector } from 'react-redux';
+import Currency from 'react-currency-formatter';
+import { selectItems, selectTotal } from '../slices/basketSlice';
+import { useSession } from 'next-auth/react';
+import { groupBy } from 'lodash';
 
 function Hover() {
   const items = useSelector(selectItems);
   const total = useSelector(selectTotal);
-  const [session] = useSession();
-  const groupedItems = Object.values(groupBy(items, "id"));
+  const { data: session, status } = useSession();
+  const groupedItems = Object.values(groupBy(items, 'id'));
 
   return (
     <div>
       <div className="flex flex-col">
         <h1 className={`text-xs "border-b "pb-2"}`}>
-          {items.length === 0 ? "Your Amazon Basket is empty." : ""}
+          {items.length === 0 ? 'Your Amazon Basket is empty.' : ''}
         </h1>
 
         {groupedItems.map((group, i) => (
@@ -29,12 +29,10 @@ function Hover() {
           </div>
         ))}
         <h2 className="whitespace-nowrap">
-          
-          Subtotal ({items.length} items):{" "}
+          Subtotal ({items.length} items):{' '}
           <span className="font-bold">
             <Currency quantity={total * 73} currency="INR" />
           </span>
-
         </h2>
       </div>
     </div>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,39 +1,39 @@
-import { getSession } from "next-auth/client";
-import Head from "next/head";
-import Banner from "../components/Banner";
-import Footer from "../components/Footer";
-import Header from "../components/Header"
-import ProductFeed from "../components/ProductFeed";
+import { getSession } from 'next-auth/react';
+import Head from 'next/head';
+import Banner from '../components/Banner';
+import Footer from '../components/Footer';
+import Header from '../components/Header';
+import ProductFeed from '../components/ProductFeed';
 
-export default function Home({products}) {
+export default function Home({ products }) {
   return (
     <div className="bg-gray-100">
       <Head>
         <title>Amazon 2.0</title>
       </Head>
-      <Header/>
+      <Header />
       <main className="max-w-screen-2xl mx-auto">
-        <Banner/>
+        <Banner />
         {/* Product feed */}
-        <ProductFeed products={products}/>
-        <Footer/>
+        <ProductFeed products={products} />
+        <Footer />
       </main>
-
     </div>
   );
 }
 
-export async function getServerSideProps(context){
-
+export async function getServerSideProps(context) {
   const session = await getSession(context);
 
-  const products = await fetch("https://fakestoreapi.com/products").then(
+  const products = await fetch('https://fakestoreapi.com/products').then(
     (res) => res.json()
   );
-  return{props:{
-    products,
-    session
-  }}
+  return {
+    props: {
+      products,
+      session,
+    },
+  };
 }
 
 // https://fakestoreapi.com/products
