0cf07d31f37ca5af26a5449b58970858d061814a
Aniket Kanere <aniket15304@gmail.com>
2022-01-21 16:25:55 +0530

    updated client

--- src/pages/api/create-checkout-session.js
+++ src/pages/api/create-checkout-session.js
@@ -1,47 +1,47 @@
-import { groupBy } from "lodash";
+import { groupBy } from 'lodash';
 
-const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
 export default async (req, res) => {
-    const { items, email } = req.body;
+  const { items, email } = req.body;
 
-    console.log(items);
-    console.log(email);
+  console.log(items);
+  console.log(email);
 
-    // @Todo : READ THE STRIPE DOCS!
+  // @Todo : READ THE STRIPE DOCS!
 
-    const groupedItems = Object.values(groupBy(items, "id"));
+  const groupedItems = Object.values(groupBy(items, 'id'));
 
-    const transformedItems = groupedItems.map((group) => ({
-        description: group[0].description,
-        quantity: group.length,
-        price_data: {
-            currency: "inr",
-            unit_amount: group[0].price * 100 *73, 
-            product_data: {
-                name: group[0].title,
-                images: [group[0].image],
-            },
-        },
-    }));
+  const transformedItems = groupedItems.map((group) => ({
+    description: group[0].description,
+    quantity: group.length,
+    price_data: {
+      currency: 'inr',
+      unit_amount: group[0].price * 100 * 73,
+      product_data: {
+        name: group[0].title,
+        images: [group[0].image],
+      },
+    },
+  }));
 
-    const session = await stripe.checkout.sessions.create({
-        payment_method_types: ["card"],
-        shipping_rates: ["shr_1Iu6iBSBHfL9pzhwFC546tT0"], // Created fees in Stripe's dashboard
-        shipping_address_collection: {
-            allowed_countries: ["GB", "US", "CA", "IN"], // RTFM!
-        },
-        line_items: transformedItems,
-        mode: "payment",
-        success_url: `${process.env.HOST}/success`,
-        cancel_url: `${process.env.HOST}/checkout`,
-        metadata: {
-            email,
-            images: JSON.stringify(items.map((item) => item.image)),
-        },
-    });
+  const session = await stripe.checkout.sessions.create({
+    payment_method_types: ['card'],
+    shipping_rates: ['shr_1KKKisSAdkSlmtvZaRphlpr5'], // Created fees in Stripe's dashboard
+    shipping_address_collection: {
+      allowed_countries: ['GB', 'US', 'CA', 'IN'], // RTFM!
+    },
+    line_items: transformedItems,
+    mode: 'payment',
+    success_url: `${process.env.HOST}/success`,
+    cancel_url: `${process.env.HOST}/checkout`,
+    metadata: {
+      email,
+      images: JSON.stringify(items.map((item) => item.image)),
+    },
+  });
 
-    console.log("session created!", session.id);
+  console.log('session created!', session.id);
 
-    res.status(200).json({ id: session.id });
+  res.status(200).json({ id: session.id });
 };
